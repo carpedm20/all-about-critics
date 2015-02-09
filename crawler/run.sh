b3d7f62a503615a914c0653c80093fc25723d43a
carpedm20 <carpedm20@gmail.com>
2015-02-09 23:41:22 +0900

    update

--- crawler/run.sh
+++ crawler/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-for i in 0 1000 2000 3000;
+for i in 1000 2000 3000;
 do
   j=1000-i
-  scrapy crawl cine -a sidx=$i -a eidx=$j -o "critic-$i".jsonlines -t jsonlines --logfile=log.txt
+  scrapy crawl cine -a sidx=$i -a eidx=$j -o "critic-new-$i".jsonlines -t jsonlines --logfile=log$i.txt &
 done
