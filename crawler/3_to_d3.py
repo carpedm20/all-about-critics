7f6ce9e7a7df8d76cf8b5d0750d0d2c9171d24a4
carpedm20 <carpedm20@gmail.com>
2015-02-10 18:33:26 +0900

    update

--- crawler/3_to_d3.py
+++ crawler/3_to_d3.py
@@ -2,12 +2,47 @@ import json
 import pandas as pd
 
 movies = json.loads(open('compact.json').read())
-data = json.loads(open('result.json').read())
+data = json.loads(open('result2.json').read())
+
+movie_dict = dict([(movie['mid'], movie) for movie in movies])
 
 new_data = []
 for key, value in data.items():
-    value['mid'] = key
+    m = movie_dict[int(key)]
+
+    value['cine'] = int(key)
+    value['director'] = m['director']
+    value['director_en'] = m['director_en']
+    value['title'] = m['title']
+    value['title_en'] = m['title_en']
+    value['time'] = m['time']
+
     new_data.append(value)
 
-movies = pd.DataFrame(movies)
 data = pd.DataFrame(new_data)
+data = data[data.metacritic.notnull()]
+
+data.poster = data.poster.str.replace('http://movie2.phinf.naver.net/','')
+data.poster = data.poster.str.replace('http://movie.phinf.naver.net/','')
+data.poster = data.poster.str.replace('/movie_image.jpg\?type=m203_290_2','')
+
+data.to_csv('movie.csv', sep=',', index=False, encoding="utf-8") 
+
+import pickle
+with open('critics.pkl','rb') as f:
+    critics = pickle.load(f)
+
+new_critics = []
+for critic in critics:
+    cid = critic.link.split('/')[-1]
+
+    for review in critic.reviews:
+        try:
+            new_critics.append((cid, review.movie.mid, review.rating, review.text))
+        except:
+            new_critics.append((cid, review.movie[0].mid, review.rating, review.text))
+
+critics = pd.DataFrame(new_critics, columns=['cid','mid','rating','text'])
+critics = critics[critics.mid.isin(data.cine)]
+
+data.to_csv('critics.csv', sep=',', index=False, encoding="utf-8")
